4def0115ad0e9d324fb0b264cb2e879efe4fcf44
Scott Bronikowski <scottbronikowski@gmail.com>
2014-04-15 11:03:35 -0400

    --Implemented signal handler in run_cameras.cpp.  Can now exit cleanly from by sending 'pkill run_cameras' either locally or over ssh.

--- root/source/camera/run_cameras.cpp
+++ root/source/camera/run_cameras.cpp
@@ -8,19 +8,35 @@
  */
 
 #include "toollib-camera.h"
+#include <csignal>
 
 using namespace FlyCapture2;
+
+//global variables -- needed for signal_handler to do cleanup
+PointGrey_t* PG;
+unsigned int numCameras;
+
+void signal_handler(int signum)
+{
+  //printf("received signal %d\n", signum);
+  PGR_StopAndCleanup(PG, numCameras);
+  printf("Image transfer stopped...exiting\n" );
+  exit(signum);
+}
+
 
 int main(int /*argc*/, char** /*argv*/)
 {
   BusManager busMgr;
-  unsigned int numCameras = PGR_Init(&busMgr);
+  //unsigned int 
+  numCameras = PGR_Init(&busMgr);
   if ( numCameras < 1 )
   {
     printf( "Insufficient number of cameras... exiting\n" );
     return -1;
   }
-  PointGrey_t* PG = new PointGrey_t[numCameras];
+  //PointGrey_t* 
+  PG = new PointGrey_t[numCameras];
   if (PGR_StartCameras(&busMgr, PG, numCameras) != 0)
   {
     printf("Error starting cameras\n");
@@ -33,7 +49,11 @@ int main(int /*argc*/, char** /*argv*/)
   }
   //setup completed
   printf("Rover: Image transfer commencing.\n");
-  
+
+  //register signal handler for termination
+  signal(SIGINT, signal_handler);
+  signal(SIGTERM, signal_handler);
+
   while(1)
   {
     for (unsigned int i = 0; i < numCameras; i++)
@@ -44,8 +64,8 @@ int main(int /*argc*/, char** /*argv*/)
     }
   }
     
-  //cleanup
-  PGR_StopAndCleanup(PG, numCameras);
-  printf( "Done!\n" );
+  //cleanup -- now done in signal_handler
+  // PGR_StopAndCleanup(PG, numCameras);
+  // printf( "Done!\n" );
   return 0;
 }
